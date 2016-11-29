4156d8f464301608acd1c7c7691e2c95c90f3550
Zero-virus <cristian.castillor@mail.udp.cl>
2016-11-29 12:56:50 -0300

    Nubes bugeadas
    Scripts e imagenes de las nubes

---
+++ Assets/Script/Cloud.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+	public float velocity;
+	private Rigidbody2D rb2d;
+
+	public float move;
+	private float initposition;
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>(); 
+		initposition = transform.position.y;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rb2d.AddForce(Vector2.up * velocity);
+
+		if(transform.position.y > initposition+ move ) {
+			//fuerza del salto
+
+			rb2d.AddForce(Vector2.up * -velocity);
+		}
+	}
+
+}
---
+++ Assets/Script/CloudX.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudX : MonoBehaviour {
+	public float velocity;
+	private Rigidbody2D rb2d;
+
+	public float move;
+	private float initposition;
+
+
+	// Use this for initialization
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>(); 
+		initposition = transform.position.x;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rb2d.AddForce(Vector2.left * velocity);
+
+		if(transform.position.x < initposition+ 30f ) {
+
+			rb2d.AddForce(Vector2.left * -velocity);
+		}
+}
+}
