ccdee89923bef5de4a18aa71b1d9f36061a2ebea
ctejadan <cristian.tejada@mail.udp.cl>
2016-12-18 16:58:23 -0300

    cambio en parte inicial

---
+++ Assets/Script/door2.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class door2 : MonoBehaviour {
+
+    public int LevelToLoad;
+    private gameMaster gm;
+    public GameObject doorSound;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<gameMaster>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.InputText.text = ("Presione [E]");
+            if (Input.GetKeyDown("e"))
+            {
+                doorSound.SetActive(true);
+                SceneManager.LoadScene("Etapa2");
+
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                SceneManager.LoadScene("Etapa2");
+                doorSound.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gm.InputText.text = (" ");
+        }
+    }
+}
