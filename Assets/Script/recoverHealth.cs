29a152e0180bf59a05ccafcbb5203992dde90d4d
ctejadan <cristian.tejada@mail.udp.cl>
2016-12-19 02:38:33 -0300

    corazones

---
+++ Assets/Script/recoverHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class recoverHealth : MonoBehaviour {
+
+    public GameObject heart;
+    public GameObject heartSound;
+    private Player player;
+    
+
+	// Use this for initialization
+	void Start () {
+   
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D col) {
+
+        if(col.CompareTag("Player"))
+        {
+            heartSound.SetActive(false);
+            player.curHealth = player.curHealth + 1;
+            heartSound.SetActive(true);
+            heart.SetActive(false);
+        }
+	
+	}
+}
